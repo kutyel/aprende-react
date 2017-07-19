0a58d3167dff7c20ba43144c70f70ff10330b4db
kutyel <flaviocorpa@gmail.com>
2017-07-19 16:04:36 +0200

    Add show/hide comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,44 +1,35 @@
 import React, { Component } from 'react'
-import replace from 'react-string-replace'
-
-import Mention from './Mention'
 
 class Comments extends Component {
-  renderComment ({ from: { full_name: name }, text }, i) {
-    return (
-      <div className='comment' key={i}>
-        <p>
-          <strong>{name}</strong>
-          {replace(text, /@(\w+)/g, (match, i) => <Mention key={i} user={match} />)}
-          <button
-            onClick={() => this.props.removeComment(this.props.params.postId, i)}
-            className='remove-comment'>&times;</button>
-        </p>
-      </div>
-    )
-  }
+  state = { showComments: true }
 
-  submit (e) {
-    e.preventDefault()
-    const { postId } = this.props.params
-    const author = this.refs.author.value
-    const comment = this.refs.comment.value
-    this.props.addComment(postId, author, comment)
-    this.refs.commentForm.reset()
-  }
+  toggleComments = () => this.setState(({ showComments }) =>
+    ({ showComments: !showComments }))
 
-  render () {
-    return (
-      <div className='comments'>
+  renderComment = ({ from: { full_name: name }, text }, i) => (
+    <div className='comment' key={i}>
+      <p>
+        <strong>{name}</strong>
+        {text}
+        <button
+          onClick={() => this.props.removeComment(this.props.params.postId, i)}
+          className='remove-comment'>&times;</button>
+      </p>
+    </div>
+  )
+
+  render = () => (
+    <div className='comments'>
+      {this.state.showComments && (<div className='comments-list'>
         {this.props.postComments.map(this.renderComment.bind(this))}
-        <form ref='commentForm' className='comment-form' onSubmit={e => this.submit(e)}>
-          <input type='text' placeholder='author' ref='author' />
-          <input type='text' placeholder='comment' ref='comment' />
-          <input type='submit' hidden />
-        </form>
+      </div>)}
+      <div className='control-buttons'>
+        <button onClick={this.toggleComments}>
+          {this.state.showComments ? 'Hide' : 'Show'} comments
+        </button>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Comments
