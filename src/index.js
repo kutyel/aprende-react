6bbd21c8bbd4615786a48d308a713de30f031be1
kutyel <flaviocorpa@gmail.com>
2017-07-19 17:24:07 +0200

    prepare exercise for day-3

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { Router, Route } from 'react-router'
+import { Router, Route, IndexRoute } from 'react-router'
 
 import './styles/style.css'
 
 import store, { history } from './store'
 
 import App from './components/App'
+import Single from './components/Single'
+import PhotoGrid from './components/PhotoGrid'
 
 const router = (
   <Provider store={store}>
     <Router history={history}>
-      <Route path='/' component={App} />
+      <Route path='/' component={App}>
+        <IndexRoute component={PhotoGrid} />
+        <Route path='/view/:postId' component={Single} />
+      </Route>
     </Router>
   </Provider>
 )
