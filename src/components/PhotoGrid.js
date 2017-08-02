0042d7fdcb1a5ab4b41a499c4476df7f2132d4e8
kutyel <flaviocorpa@gmail.com>
2017-08-02 16:37:11 +0200

    Fetch posts with async/await

--- src/components/PhotoGrid.js
+++ src/components/PhotoGrid.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Photo from './Photo'
 
-const PhotoGrid = props => (
-  <div className='photo-grid'>
-    {props.posts.map((p, i) => <Photo key={i} i={i} post={p} {...props} />)}
-  </div>
-)
+const API_URL = 'https://gist.githubusercontent.com/kutyel/8f2d287ceb0a04bf3edb2e435b07f61d/raw/623cfaee5cfae55783c4f22366f6e4db92a98e2d/catstagram.json'
+
+class PhotoGrid extends Component {
+  state = { posts: [] }
+
+  componentDidMount = async () => {
+    try {
+      const resp = await fetch(API_URL)
+      const posts = await resp.json()
+      this.setState({ posts })
+    } catch (err) {
+      console.error(`Error fetching kittens: ${err}`)
+    }
+  }
+
+  render = () => (
+    <div className='photo-grid'>
+      {this.state.posts.map((p, i) => <Photo key={i} i={i} post={p} {...this.props} />)}
+    </div>
+  )
+}
 
 export default PhotoGrid
