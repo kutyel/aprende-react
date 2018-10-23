0c9c6814c330f5855e11f142638cd1bfac8d3690
kutyel <flavio.corparios@ext.soprasteria.com>
2018-10-23 10:51:23 +0200

    Clarify exercise 🙌

--- src/components/PhotoGrid.js
+++ src/components/PhotoGrid.js
@@ -2,17 +2,14 @@ import React from 'react'
 
 // TODO: importar el componente `Photo`
 
-const PhotoGrid = props => (
-  <div className='photo-grid'>
+export default ({ posts, ...props }) => (
+  <div className="photo-grid">
     {/**
      * TODO: renderizar aquí el array de fotos a partir de un JSON
      *
      * Pistas:
-     * - el array de datos está en `props.posts`
      * - hay que transformar el array de JS en un array de `Photo`
      * - el componente `Photo` *requiere*: `key`, `i` (índice), `post`, y `{...props}`
      */}
   </div>
 )
-
-export default PhotoGrid
