5ecc632dee3d0ac5a414760f767e29a01a2a142d
kutyel <flavio.corparios@ext.soprasteria.com>
2018-11-14 11:50:22 +0100

    Simplify store.js ⚡️

--- src/store.js
+++ src/store.js
@@ -3,25 +3,15 @@ import { createStore, compose } from 'redux'
 import { browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 
-import reducer from './reducers/index'
-
+import reducer from './reducers'
 import posts from './data/posts.json'
 import comments from './data/comments.json'
 
 const defaultState = { posts, comments }
-
 const enhancers = compose(
   window.devToolsExtension ? window.devToolsExtension() : identity
 )
-
 const store = createStore(reducer, defaultState, enhancers)
 
 export const history = syncHistoryWithStore(browserHistory, store)
-
-if (module.hot) {
-  module.hot.accept('./reducers/', () =>
-    store.replaceReducer(require('./reducers/index').default)
-  )
-}
-
 export default store
