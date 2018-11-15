23ecad5c5e87ed33737d35f34d0453cd22e714ad
kutyel <flavio.corparios@ext.soprasteria.com>
2018-11-15 09:29:30 +0100

    Use redux-logger

--- src/store.js
+++ src/store.js
@@ -1,15 +1,18 @@
-import { identity } from 'ramda'
-import { createStore, compose } from 'redux'
+import { createLogger } from 'redux-logger'
 import { browserHistory } from 'react-router'
+import { createStore, applyMiddleware } from 'redux'
 import { syncHistoryWithStore } from 'react-router-redux'
 
 import reducer from './reducers'
 import posts from './data/posts.json'
 import comments from './data/comments.json'
 
-const defaultState = { posts, comments }
-const composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(reducer, defaultState, composeEnchancers(identity))
+const initialState = { posts, comments }
+const store = createStore(
+  reducer,
+  initialState,
+  applyMiddleware(createLogger())
+)
 
 export const history = syncHistoryWithStore(browserHistory, store)
 export default store
